b72f3d3fc308f202e5bf36781605a54fd380b7fb
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-10 09:26:59 +0200

    feat: add ImageField into the Author model

--- reviews/models.py
+++ reviews/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Author(AbstractUser):
     bio = models.CharField(max_length=150)
-    # avatar = models.ImageField(upload_to='images/')
+    avatar = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
         return self.username
