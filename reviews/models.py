4e11bc0fe9e3f8a88c2053366c78513effc77bf8
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-10 10:54:17 +0200

    fix: remove 'null=True' from 'bio' and 'avatar' fields in Author model

--- reviews/models.py
+++ reviews/models.py
@@ -4,9 +4,8 @@ from .validators import validate_avatar, avatar_extension_validator
 
 
 class Author(AbstractUser):
-    bio = models.CharField(max_length=150)
+    bio = models.CharField(max_length=150, blank=True)
     avatar = models.ImageField(upload_to='images/', 
-                               null=True,
                                blank=True,
                                validators=[validate_avatar,
                                            avatar_extension_validator]
