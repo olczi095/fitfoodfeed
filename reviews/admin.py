0568f65e9caf3f358a5840474df38d7b003e544d
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-11 10:16:39 +0200

    feat(reviews): add Post model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author
+from .models import Author, Post
 
 
 @admin.register(Author)
@@ -10,3 +10,17 @@ class AuthorAdmin(admin.ModelAdmin):
         return obj.username
 
     display_author.short_description = 'Author'
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'pub_date', 'status']
+    list_filter = ['status', 'author']
+    search_fields = ['title', 'meta_description', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+
+    def display_author(self, obj):
+        return obj.username
+
+    display_author.short_description = 'Author'
+
--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 from .validators import validate_avatar_type, validate_avatar_dimensions, validate_avatar_size
 
 
@@ -14,3 +15,27 @@ class Author(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+class Post(models.Model):
+    STATUS_CHOICES = [
+        (0, 'Draft'),
+        (1, 'Prepared to publish'),
+        (2, 'Published')
+    ]
+
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    pub_date = models.DateField(default=timezone.now)
+    author = models.ForeignKey(Author, on_delete=models.SET_DEFAULT, default='Anonymous', related_name='review_posts')
+    meta_description = models.CharField(max_length=150, blank=True)
+    body = models.TextField()
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+
+
+    class Meta:
+        ordering = ['-pub_date']
+
+
+    def __str__(self):
+        return self.title
