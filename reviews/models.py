627cf7946b6774af80d49bbfc9a4d1804558d084
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-12 11:24:11 +0200

    refactor: rename the name of media directory (images to avatars)

--- reviews/models.py
+++ reviews/models.py
@@ -6,7 +6,7 @@ from .validators import validate_avatar_type, validate_avatar_dimensions, valida
 
 class Author(AbstractUser):
     bio = models.CharField(max_length=150, blank=True)
-    avatar = models.ImageField(upload_to='images/', 
+    avatar = models.ImageField(upload_to='avatars/', 
                                blank=True,
                                validators=[validate_avatar_type, 
                                            validate_avatar_dimensions, 
