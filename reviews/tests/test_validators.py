e5b40e89f1b7d21b1284f6fce7dd86275e4b0587
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-17 10:22:16 +0200

    test: add AvatarDimensionsValidatorTestCase
    Create tests with various dimensions of passing image (valid and invalid) for 'validate_avatar_dimensions' function from validators.py in reviews app.

--- reviews/tests/test_validators.py
+++ reviews/tests/test_validators.py
@@ -5,7 +5,7 @@ from django.forms import ValidationError
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from reviews.validators import (validate_avatar_type)
+from reviews.validators import (validate_avatar_type, validate_avatar_dimensions)
 
 
 class AvatarTypeValidatorTestCase(TestCase):
@@ -37,3 +37,41 @@ class AvatarTypeValidatorTestCase(TestCase):
         image_with_invalid_type = SimpleUploadedFile("test_image.gif", image_data, content_type="image/gif")
         with self.assertRaises(ValidationError):
             validate_avatar_type(image_with_invalid_type)
+
+
+class AvatarDimensionsValidatorTestCase(TestCase):
+    def test_image_with_min_valid_dimensions(self):
+        image = Image.new("RGB", (100, 100), (255, 255, 255))
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, "JPEG")
+        self.assertEqual(validate_avatar_dimensions(image), None)
+
+    def test_image_with_max_valid_dimensions(self):
+        image = Image.new("RGB", (500, 500), (255, 255, 255))
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, "JPEG")
+        self.assertEqual(validate_avatar_dimensions(image), None)
+
+    def test_image_too_small_width_returns_error(self):
+        image = Image.new("RGB", (50, 350), (255, 255, 255))
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, "JPEG")
+        self.assertRaises(ValidationError, validate_avatar_dimensions, image)
+
+    def test_image_with_too_large_width_returns_error(self):
+        image = Image.new("RGB", (600, 350), (255, 255, 255))
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, "JPEG")
+        self.assertRaises(ValidationError, validate_avatar_dimensions, image)
+
+    def test_image_with_too_small_height_returns_error(self):
+        image = Image.new("RGB", (350, 50), (255, 255, 255))
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, "JPEG")
+        self.assertRaises(ValidationError, validate_avatar_dimensions, image)
+
+    def test_image_with_too_large_height_returns_error(self):
+        image = Image.new("RGB", (350, 600), (255, 255, 255))
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, "JPEG")
+        self.assertRaises(ValidationError, validate_avatar_dimensions, image)
