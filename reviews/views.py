b366f747acf1afa4049720d1206e1a63f1f5947f
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-20 16:09:33 +0200

    feat: display the title and body of posts on the homepage

--- reviews/views.py
+++ reviews/views.py
@@ -8,3 +8,9 @@ from reviews.models import Post
 class PostListView(ListView):
     model = Post
     template_name = 'reviews/home.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_queryset(self):
+        return super().get_queryset().order_by('-pub_date')
+    
