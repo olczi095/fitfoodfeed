1c769230eb13b597bd89eec63652bcf692b3ee28
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-20 12:35:13 +0200

    feat: set the home page

--- reviews/views.py
+++ reviews/views.py
@@ -1,1 +1,10 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView
+
+from reviews.models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'reviews/home.html'
