0978d95f02c0fb742fc285e8f7ccd3522b8f636a
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-23 16:37:38 +0200

    feat: display pagination with the arrow images on the homepage

--- reviews/views.py
+++ reviews/views.py
@@ -9,7 +9,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'reviews/home.html'
     context_object_name = 'posts'
-    # paginate_by = 3
+    paginate_by = 3
 
     def get_queryset(self):
         return super().get_queryset().order_by('-pub_date')
