dbb91f96f4eeb45a5f4366b704c63c66f93ed68a
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-28 08:51:16 +0200

    fix: move an Admin registration to the accounts app

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
-from .models import Author, Post
-
-
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['display_author', 'email', 'bio']
-
-    def display_author(self, obj):
-        return obj.username
-
-    display_author.short_description = 'Author'
+from .models import Post
 
 
 @admin.register(Post)
