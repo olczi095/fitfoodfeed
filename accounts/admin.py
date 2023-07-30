476003cca3028abd665e0cb2fd6be4d3678257af
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-07-30 14:45:33 +0200

    feat: display user groups on the admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,9 +4,14 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['display_user', 'email', 'bio']
+    list_display = ['display_user', 'display_groups', 'email', 'bio']
 
     def display_user(self, obj):
         return obj.username
+    
+    def display_groups(self, obj):
+        user_groups = [group.name for group in obj.groups.all()]
+        return ', '.join(user_groups)
 
+    display_groups.short_description = 'Role'
     display_user.short_description = 'User'
