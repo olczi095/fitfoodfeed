1ac47e346024b366c385856f5af49310dfc011bb
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-08-04 10:13:19 +0200

    feat: display an error message after invalid login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.contrib import messages
+
 from .forms import CustomUserCreationForm
 
 
@@ -15,3 +17,7 @@ class RegisterView(CreateView):
 class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('app_reviews:home')
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password.')
+        return self.render_to_response(self.get_context_data(form=form))
