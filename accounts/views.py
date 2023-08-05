64d570f5f0399faefecca749f0586a64559378c1
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-08-05 10:14:06 +0200

    feat: add success message after successful registration

--- accounts/views.py
+++ accounts/views.py
@@ -2,17 +2,24 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .forms import CustomUserCreationForm
 
 
-class RegisterView(CreateView):
+class RegisterView(SuccessMessageMixin, CreateView):
     template_name = 'registration/register.html'
     form_class = CustomUserCreationForm
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data['username']
+        messages.success(self.request, f"Registration Successful! Welcome {username}, you are now logged in.")
+        return response
+
     def get_success_url(self):
         return reverse_lazy('app_reviews:home')
-    
+
 
 class CustomLoginView(LoginView):
     def get_success_url(self):
