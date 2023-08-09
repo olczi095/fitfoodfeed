c3ad5f10028729b9f87457cc72f5b3b8ea06eeb6
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-08-09 10:18:43 +0200

    feat: add labels for PostForm

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,3 +6,10 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'meta_description', 'body', 'image', 'slug']
+        labels = {
+            'title': 'Title',
+            'meta_description': 'Meta Description',
+            'body': 'Body',
+            'image': 'Image',
+            'slug': 'Slug'
+        }
