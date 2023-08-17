24be2f309d4c06fde2b5af0d1808bcecf76b4383
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-08-17 10:26:23 +0200

    feat: add get_queryset and tag_list methods to PostAdmin

--- reviews/admin.py
+++ reviews/admin.py
@@ -4,7 +4,13 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'pub_date', 'status']
+    list_display = ['title', 'author', 'slug', 'tag_list', 'pub_date', 'status']
     list_filter = ['status', 'author']
     search_fields = ['title', 'meta_description', 'body']
     prepopulated_fields = {'slug': ('title',)}
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+    
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
