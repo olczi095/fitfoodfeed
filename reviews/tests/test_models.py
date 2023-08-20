fc1c2e65826abfb0c9f0d99e21ba69ef34e78657
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-08-20 11:28:48 +0200

    test: ensure existence of the category field in Post model

--- reviews/tests/test_models.py
+++ reviews/tests/test_models.py
@@ -23,7 +23,8 @@ class PostModelTestCase(TestCase):
             slug='test_slug',
             pub_date=timezone.now().date(),
             author=self.author,
-            body='test_body'
+            body='test_body',
+            category='other'
         )
         self.second_post = Post.objects.create(
             title='second_test_title',
@@ -41,6 +42,7 @@ class PostModelTestCase(TestCase):
         self.assertEqual(self.post.meta_description, '')
         self.assertEqual(self.post.body, 'test_body')
         self.assertEqual(self.post.status, 'DRAFT')
+        self.assertEqual(self.post.category, 'Other')
 
     def test_status_choices(self):
         expected_status_choices = [
