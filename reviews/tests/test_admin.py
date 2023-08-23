0d6478d3738f719e293232e8dc4e3d97ca917940
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-08-23 13:26:25 +0200

    test: ensure automatic author assignment in PostAdmin

--- reviews/tests/test_admin.py
+++ reviews/tests/test_admin.py
@@ -4,13 +4,15 @@ from taggit.models import Tag
 from accounts.models import User
 from reviews.models import Post
 from reviews.admin import PostAdmin
+from unittest.mock import Mock
 
 
 class PostAdminTestCase(TestCase):
     def setUp(self):
         self.author = User.objects.create(
             username='test_user',
-            password='test_password'
+            password='test_password',
+            is_staff=True,
         )
         self.review = Post.objects.create(
             title='My Review',
@@ -31,3 +33,15 @@ class PostAdminTestCase(TestCase):
         postModelAdmin = PostAdmin(model=Post, admin_site=AdminSite())
         expected_tag_list = 'chocolate, bars'
         self.assertEqual(postModelAdmin.tag_list(self.review), expected_tag_list)
+
+    def test_assign_author_automatically_to_creating_post(self):
+        self.another_review = Post(
+            title='Another Review',
+            author=self.author,
+            body='This is the body of my another review.',
+        )
+        postModelAdmin = PostAdmin(model=Post, admin_site=AdminSite())
+        request = Mock(user=self.author)
+        postModelAdmin.save_model(request, self.another_review, PostAdmin.form, change=False)
+        new_review = Post.objects.get(pk=self.another_review.pk)
+        self.assertEqual(new_review.author, self.author)
