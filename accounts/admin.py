642b7b61319e20f95304a811ade271d64a133d68
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-08-25 16:31:55 +0200

    feat(accounts): add fieldsets to UserAdmin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,32 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['display_user', 'display_groups', 'email', 'is_author', 'is_staff',]
+    fieldsets = [
+        (
+            'Identification Data',
+            {
+                'fields': ['username', 'password', 'first_name', 'last_name', 'email'] 
+            }
+        ),
+        (
+            'Administration Properties',
+            {
+                'fields': ['is_active', 'is_superuser', 'is_staff', 'is_author']
+            }
+        ),
+        (
+            'Permission-related Fields',
+            {
+                'fields': ['groups', 'user_permissions']
+            }
+        ),
+        (
+            'Additional Informations',
+            {
+                'fields': ['avatar', 'bio']
+            }
+        )
+    ]
 
     def display_user(self, obj):
         return obj.username
