d90b8ceb57c175c9baa37791300f33a3f212ce5c
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-08-28 10:22:54 +0200

    test: test adding permissions to author user

--- accounts/tests/test_signals.py
+++ accounts/tests/test_signals.py
@@ -14,3 +14,29 @@ class SignalsTestCase(TestCase):
         )
         user_has_admin_group = admin_group in user.groups.all()
         self.assertTrue(user_has_admin_group)
+
+    def test_author_add_permissions(self):
+        test_author = User.objects.create_user(
+            username='author', 
+            password='xyz',
+            is_author=True
+        )
+        self.assertTrue(test_author.is_author)
+
+        expected_permissions = ['add_post', 'view_post']
+        test_author_permissions = [permission.codename for permission in test_author.user_permissions.all()]
+        for expected_permission in expected_permissions:
+            self.assertIn(expected_permission, test_author_permissions)
+
+    def test_non_author_no_permissions(self):
+        test_author = User.objects.create_user(
+            username='author', 
+            password='xyz',
+            is_author=False
+        )
+        self.assertFalse(test_author.is_author)
+
+        expected_permissions = ['add_post', 'view_post']
+        test_author_permissions = [permission.codename for permission in test_author.user_permissions.all()]
+        for expected_permission in expected_permissions:
+            self.assertNotIn(expected_permission, test_author_permissions)
