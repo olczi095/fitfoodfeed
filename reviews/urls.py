b1b8cc14b63b368aed8043d0f93e670fe41cc8d9
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-08-29 12:11:41 +0200

    feat: display update review page

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, TaggedPostsListView, TagsListView, CategoryListView
+from .views import PostListView, PostDetailView, PostCreateView, TaggedPostsListView, TagsListView, CategoryListView, PostUpdateView
 
 app_name = 'app_reviews'
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('tags/', TagsListView.as_view(),name='tags'),
     path('create-review/', PostCreateView.as_view(), name='create_review'),
+    path('update-review/<int:pk>/', PostUpdateView.as_view(), name='update_review'),
     path('category/<str:category_name>/', CategoryListView.as_view(), name='category'),
     path('tag/<str:tag_name>/', TaggedPostsListView.as_view(), name='tag'),
     path('<slug:slug>/', PostDetailView.as_view(), name='review'),
