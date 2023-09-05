ce496e5a73480d88ad26884c6ce34a32473b4faa
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-09-05 20:11:03 +0200

    fix: remove the incorrect test

--- accounts/tests/test_signals.py
+++ accounts/tests/test_signals.py
@@ -4,17 +4,6 @@ from accounts.models import User
 
 
 class SignalsTestCase(TestCase):
-    def test_adding_superuser_to_admin_group(self):
-        admin_group, created = Group.objects.get_or_create(name='admin')
-        user = User.objects.create(
-            username='test_user', 
-            password='test_password',
-            bio='test_bio',
-        )
-        user.is_superuser = True
-        user_has_admin_group = admin_group in user.groups.all()
-        self.assertTrue(user_has_admin_group)
-
     def test_author_add_permissions(self):
         test_author = User.objects.create_user(
             username='author', 
