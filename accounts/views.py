dc45517dec55ffe86bbc67d9eae887bd729d10f5
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-10-16 12:19:05 +0200

    refactor(accounts): update type hints in views

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
-from typing import Dict, Type
+from typing import Type
 
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy, reverse
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.forms import BaseForm
@@ -15,15 +16,15 @@ class RegisterView(SuccessMessageMixin, CreateView):
     template_name: str = 'registration/register.html'
     form_class: Type[CustomUserCreationForm] = CustomUserCreationForm
     success_url: str = reverse_lazy('app_accounts:login')
-    success_message = f"Registration Successful! Now you can log in."
+    success_message: str = f"Registration Successful! Now you can log in."
 
 
 class CustomLoginView(LoginView):
     def get_success_url(self) -> str:
         return reverse('app_reviews:home')
     
-    def form_valid(self, form):
-        username = form.cleaned_data.get('username')
+    def form_valid(self, form: AuthenticationForm) -> HttpResponse:
+        username: str | None = form.cleaned_data.get('username')
         messages.success(self.request, f"Hello, <strong>{username}</strong>!")
         return super().form_valid(form)
     
