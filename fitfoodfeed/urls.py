193f0c0c40bffff91f75885a8907c303306d8246
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-11-01 12:35:41 +0100

    refactor: change base URL for 'reviews' app

--- fitfoodfeed/urls.py
+++ fitfoodfeed/urls.py
@@ -5,7 +5,7 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('reviews.urls')),
+    path('reviews/', include('reviews.urls')),
     path('accounts/', include('accounts.urls'), name='accounts')
 ]
 
