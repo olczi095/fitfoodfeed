905955e91ac5905189a2fd010125dc76b813b49f
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-11-23 10:03:21 +0100

    feat: implement AJAX reply form
    The reply comment form will be displaying after clicking the 'reply' button.

---
+++ reviews/static/reviews/js/comment_reply.js
@@ -0,0 +1,9 @@
+function toggleCommentFormReply(button) {
+    let replyForm = button.nextElementSibling;
+
+    if (replyForm.style.display === 'none' || replyForm.style.display === '') {
+        replyForm.style.display = 'block';
+    } else {
+        replyForm.style.display = 'none';
+    }
+}
