c6c4182134e9257c5e2e5e62fb24c5b6234bd24f
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-12-05 10:21:51 +0100

    refactor: optimize JavaScript code (remove 'onclick' elements)

--- reviews/static/reviews/js/comment_reply.js
+++ reviews/static/reviews/js/comment_reply.js
@@ -1,9 +1,28 @@
-function toggleCommentFormReply(button) {
-    let replyForm = button.nextElementSibling;
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.comment-reply-button').forEach(commentReplyButton => {
+            commentReplyButton.addEventListener('click', function() {
+                toggleCommentFormReply(this);
+                toggleReplyButtonColor(this);
+        });
+    });
+});
+
+function toggleCommentFormReply(replyButton) {
+    let replyForm = replyButton.nextElementSibling;
 
     if (replyForm.style.display === 'none' || replyForm.style.display === '') {
         replyForm.style.display = 'block';
     } else {
         replyForm.style.display = 'none';
+    }
+}
+
+function toggleReplyButtonColor(replyButton) {
+    if (replyButton) {
+        if (!replyButton.classList.contains('comment-reply-button-blue')) {
+            replyButton.classList.add('comment-reply-button-blue');
+        } else {
+            replyButton.classList.remove('comment-reply-button-blue');
+        }
     }
 }
