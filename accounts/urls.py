97721dd5b6d42f19f8df4cd26a1a16f7e6033358
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-12-10 22:40:11 +0100

    feat: implement password reset functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import (
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 
 from .views import RegisterView, CustomLoginView
 
@@ -10,4 +16,23 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('password-reset/', PasswordResetView.as_view(
+        template_name = 'password_reset/password_reset.html',
+        email_template_name = 'password_reset/password_reset_email.html',
+        success_url = reverse_lazy('app_accounts:password_reset_done')
+    ), name='password_reset'),
+
+    path('password-reset/done/', PasswordResetDoneView.as_view(
+        template_name = 'password_reset/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('password-reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name = 'password_reset/password_reset_confirm.html',
+        success_url = reverse_lazy('app_accounts:password_reset_complete')
+    ), name='password_reset_confirm'),
+
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(
+        template_name = 'password_reset/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
--- fitfoodfeed/settings.py
+++ fitfoodfeed/settings.py
@@ -126,3 +126,20 @@ LOGIN_REDIRECT_URL = 'app_reviews:home'
 LOGOUT_REDIRECT_URL = 'app_reviews:home'
 
 TAGGIT_CASE_INSENSITIVE = True
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# # Pass your e-mail address
+# EMAIL_HOST_USER = 'your_email@mail.com'
+# # Pass your e-mail password
+# EMAIL_HOST_PASSWORD = 'yourpassword'
+
+# Pass your e-mail address
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# Pass your e-mail password
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
